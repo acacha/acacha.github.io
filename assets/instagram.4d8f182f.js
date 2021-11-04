c555b70e5145cff88a8ab594e7a0888f893730ca
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2021-11-04 13:35:49 +0100

    deploy

---
+++ assets/instagram.4d8f182f.js
@@ -0,0 +1,1 @@
+import{I as a}from"./Instagram.c14853bd.js";import{f as p}from"./vendor.9ca9d2e8.js";const r=p(a);r.mount("#app");
--- assets/instagram.7021db84.js
+++
@@ -1,1 +0,0 @@
-import{I as a}from"./Instagram.e1c5603e.js";import{f as p}from"./vendor.226cac69.js";const r=p(a);r.mount("#app");
